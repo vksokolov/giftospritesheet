50c0ef11f463fb6cdd63400e4dbd22862170990a
vksokolov <vladislav.k.sokolov@gmail.com>
2017-01-18 23:54:44 +0300

    Initial commit

---
+++ GifToSpritesheet/GifToSpritesheet/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace GifToSpritesheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rowUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colUpDown = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rowUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gif path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(110, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(586, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(13, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(683, 86);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create Spritesheet!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rowUpDown
+            // 
+            this.rowUpDown.Location = new System.Drawing.Point(150, 69);
+            this.rowUpDown.Name = "rowUpDown";
+            this.rowUpDown.Size = new System.Drawing.Size(64, 22);
+            this.rowUpDown.TabIndex = 3;
+            this.rowUpDown.ValueChanged += new System.EventHandler(this.upDown_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Row";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Col";
+            // 
+            // colUpDown
+            // 
+            this.colUpDown.Location = new System.Drawing.Point(150, 97);
+            this.colUpDown.Name = "colUpDown";
+            this.colUpDown.Size = new System.Drawing.Size(64, 22);
+            this.colUpDown.TabIndex = 5;
+            this.colUpDown.ValueChanged += new System.EventHandler(this.upDown_ValueChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 21);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Auto";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 69);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 21);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Custom";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 125);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(684, 126);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 355);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.colUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rowUpDown);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Gif to spritesheet";
+            ((System.ComponentModel.ISupportInitialize)(this.rowUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown rowUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown colUpDown;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ GifToSpritesheet/GifToSpritesheet/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GifToSpritesheet
+{
+    public partial class Form1 : Form
+    {
+        string pathToGif;
+        Bitmap spriteSheet;
+        AnimatedGif gifImg;
+        int gifWidth;
+        int gifHeight;
+        int rows;
+        int cols;
+        int frameCount;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathToGif = openFileDialog1.FileName;
+                button2.Enabled = true;
+
+
+                gifImg = new AnimatedGif(pathToGif);
+                frameCount = gifImg.Images.Count;
+                gifWidth = gifImg.Images[0].Image.Width;
+                gifHeight = gifImg.Images[0].Image.Height;
+                rows = (int)Math.Sqrt(frameCount);
+                cols = rows;
+                if ((float)gifWidth / gifHeight > 1)
+                {
+                    rows++;
+                }
+                else
+                {
+                    cols++;
+                }
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            spriteSheet = new Bitmap(gifWidth * rows, gifHeight * cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i + j * rows < frameCount)
+                        CopyRegionIntoImage(new Bitmap(gifImg.Images[i + j * rows].Image), new Rectangle(0, 0, gifWidth, gifHeight), ref spriteSheet, new Rectangle(gifWidth * i, gifHeight * j, gifWidth, gifHeight));
+                }
+            }
+            System.Drawing.Imaging.Encoder pointFilter = System.Drawing.Imaging.Encoder.Quality;
+            ImageCodecInfo pngEncoder = GetEncoder(ImageFormat.Png);
+            EncoderParameters parameters = new EncoderParameters(1);
+            parameters.Param[0] = new EncoderParameter(pointFilter, 0L);
+            spriteSheet.Save((new FileInfo(pathToGif)).Name + "_spriteSheet.png", pngEncoder, parameters);
+            spriteSheet.Dispose();
+            richTextBox1.AppendText("Finished!");
+        }
+
+        private static void CopyRegionIntoImage(Bitmap srcBitmap, Rectangle srcRegion, ref Bitmap destBitmap, Rectangle destRegion)
+        {
+            using (Graphics grD = Graphics.FromImage(destBitmap))
+            {
+                grD.DrawImage(srcBitmap, destRegion, srcRegion, GraphicsUnit.Pixel);
+            }
+        }
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled = radioButton2.Checked;
+            label2.Enabled = radioButton2.Checked;
+            rowUpDown.Enabled = radioButton2.Checked;
+            colUpDown.Enabled = radioButton2.Checked;
+            if (radioButton1.Checked)
+            {
+                rowUpDown.Value = rows;
+                colUpDown.Value = cols;
+            }
+        }
+
+        private void upDown_ValueChanged(object sender, EventArgs e)
+        {
+            int c = (int)colUpDown.Value;
+            int r = (int)rowUpDown.Value;
+            if(radioButton2.Checked)
+            if (c * r < frameCount)
+            {
+                richTextBox1.AppendText("\nOnly " + (c * r) + " frames will be saved. Add columns/rows if you want to save all animation");
+            }
+        }
+    }   
+        public class AnimatedGif
+        {
+            private List<AnimatedGifFrame> mImages = new List<AnimatedGifFrame>();
+            PropertyItem mTimes;
+            public AnimatedGif(string path)
+            {
+                try
+                {
+                    Image img = Image.FromFile(path);
+                    int frames = img.GetFrameCount(FrameDimension.Time);
+                    if (frames <= 1) throw new ArgumentException("Image not animated");
+                    byte[] times = img.GetPropertyItem(0x5100).Value;
+                    int frame = 0;
+                    for (;;)
+                    {
+                        int dur = BitConverter.ToInt32(times, 4 * frame);
+                        mImages.Add(new AnimatedGifFrame(new Bitmap(img), dur));
+                        if (++frame >= frames) break;
+                        img.SelectActiveFrame(FrameDimension.Time, frame);
+                    }
+                    img.Dispose();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            public List<AnimatedGifFrame> Images { get { return mImages; } }
+        }
+
+        public class AnimatedGifFrame
+        {
+            private int mDuration;
+            private Image mImage;
+            internal AnimatedGifFrame(Image img, int duration)
+            {
+                mImage = img; mDuration = duration;
+            }
+            public Image Image { get { return mImage; } }
+            public int Duration { get { return mDuration; } }
+        }
+    }
+
